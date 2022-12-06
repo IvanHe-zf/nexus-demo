6fd2f2013fa8e00990eaa28bfe5db19602256158
NCF-JS <584841966@qq.com>
2022-12-06 14:41:50 +0800

    提交一下看看

---
+++ src/test/java/com/itmuch/nexusdemo/NexusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmuch.nexusdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
